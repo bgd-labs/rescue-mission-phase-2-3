d41b39aebfa64589096c793b37f85f4abbf9ab95
Harsh Pandey <harshsatishpandey@gmail.com>
2023-05-11 22:09:04 +0530

    feat: add some more networks and tokens

--- js-scripts/generate-address-value-maps.ts
+++ js-scripts/generate-address-value-maps.ts
@@ -2,7 +2,7 @@ import { BigNumber, Event, providers } from 'ethers';
 import fs from 'fs';
 import { ChainId } from '@aave/contract-helpers';
 import { fetchLabel, wait } from './label-map';
-import { AaveV2Avalanche, AaveV2Ethereum, AaveV2Polygon, AaveV3Ethereum, AaveV3Polygon } from "@bgd-labs/aave-address-book";
+import { AaveV2Avalanche, AaveV2Ethereum, AaveV2EthereumAMM, AaveV2Polygon, AaveV3Arbitrum, AaveV3Avalanche, AaveV3Ethereum, AaveV3Fantom, AaveV3Harmony, AaveV3Optimism, AaveV3Polygon } from "@bgd-labs/aave-address-book";
 import { IERC20__factory } from './typechain/IERC20__factory';
 import { LendingPoolFactory } from './typechain/LendingPoolFactory';
 import { L2Pool__factory } from './typechain/L2Pool__factory';
@@ -13,18 +13,31 @@ import { LendingPool } from './typechain/LendingPool';
 import TOKENS_ETH from './assets/ethTokens.json';
 import TOKENS_POL from './assets/polTokens.json';
 import TOKENS_AVA from './assets/avaTokens.json';
-import V2_POL_A_TOKENS from './assets/v2PolATokens.json';
-import V3_POL_A_TOKENS from './assets/v2PolATokens.json';
-import V2_AVA_A_TOKENS from './assets/v2PolATokens.json';
-import V2AMM_ETH_A_TOKENS from './assets/v2AmmATokens.json';
+import TOKENS_OPT from './assets/optTokens.json';
+import TOKENS_ARB from './assets/arbTokens.json';
+import TOKENS_HAR from './assets/harTokens.json';
+import TOKENS_FAN from './assets/fanTokens.json';
 import V2_ETH_A_TOKENS from './assets/v2EthATokens.json';
+import V2_POL_A_TOKENS from './assets/v2PolATokens.json';
+import V2_AVA_A_TOKENS from './assets/v2AvaATokens.json';
+import V2AMM_ETH_A_TOKENS from './assets/v2AmmATokens.json';
 import V3_ETH_A_TOKENS from './assets/v3EthATokens.json';
+import V3_POL_A_TOKENS from './assets/v2PolATokens.json';
+import V3_AVA_A_TOKENS from './assets/v3AvaATokens.json';
+import V3_OPT_A_TOKENS from './assets/v3OptATokens.json';
+import V3_ARB_A_TOKENS from './assets/v3ArbATokens.json';
+import V3_HAR_A_TOKENS from './assets/v3HarATokens.json';
+import V3_FAN_A_TOKENS from './assets/v3FanATokens.json';
 const amountsFilePath = `./js-scripts/maps/amountsByContract.txt`;
 
 const JSON_RPC_PROVIDER = {
   [ChainId.mainnet]: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
   [ChainId.polygon]: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+  [ChainId.optimism]: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+  [ChainId.arbitrum_one]: `https://arbitrum-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
   [ChainId.avalanche]: process.env.RPC_AVALANCHE,
+  [ChainId.harmony]: process.env.RPC_HARMONY,
+  [ChainId.fantom]: process.env.RPC_FANTOM,
 };
 
 enum AaveMarket { v1, v2, v2Amm, v3 };
@@ -103,8 +116,23 @@ async function fetchTxns(
       case ChainId.polygon:
         v3PoolContract = L2Pool__factory.connect(AaveV3Polygon.POOL, provider);
         break;
+      case ChainId.avalanche:
+        v3PoolContract = L2Pool__factory.connect(AaveV3Avalanche.POOL, provider);
+        break;
+      case ChainId.optimism:
+        v3PoolContract = L2Pool__factory.connect(AaveV3Optimism.POOL, provider);
+        break;
+      case ChainId.arbitrum_one:
+        v3PoolContract = L2Pool__factory.connect(AaveV3Arbitrum.POOL, provider);
+        break;
+      case ChainId.harmony:
+        v3PoolContract = L2Pool__factory.connect(AaveV3Harmony.POOL, provider);
+        break;
+      case ChainId.fantom:
+        v3PoolContract = L2Pool__factory.connect(AaveV3Fantom.POOL, provider);
+        break;
       default:
-        throw Error('Invalid network for v3 market');
+        throw Error(`Invalid network for v3 market. network: ${network}`);
     }
     const repayEvent = v3PoolContract.filters.Repay(getUnderlyingToken(to), null, null, null);
     const supplyEvent = v3PoolContract.filters.Supply(getUnderlyingToken(to), null, null, null, null);
@@ -118,19 +146,22 @@ async function fetchTxns(
     return [...repayEvents, ...supplyEvents, ...liqCallEvents, ...flashloanEvents];
   }
 
-  async function getV2ATokensEventsToFilterOut(fromBlock: number, toBlock: number): Promise<Event[]> {
+  async function getV2ATokensEventsToFilterOut(fromBlock: number, toBlock: number, isAmm?: boolean): Promise<Event[]> {
     let v2PoolContract: LendingPool;
     switch (network) {
       case ChainId.mainnet:
-        v2PoolContract = LendingPoolFactory.connect(AaveV2Ethereum.POOL, provider);
+        v2PoolContract = isAmm ?
+          v2PoolContract = LendingPoolFactory.connect(AaveV2EthereumAMM.POOL, provider) :
+          v2PoolContract = LendingPoolFactory.connect(AaveV2Ethereum.POOL, provider);
         break;
       case ChainId.polygon:
         v2PoolContract = LendingPoolFactory.connect(AaveV2Polygon.POOL, provider);
         break;
       case ChainId.avalanche:
         v2PoolContract = LendingPoolFactory.connect(AaveV2Avalanche.POOL, provider);
+        break;
       default:
-        throw Error('Invalid network for v2 market');
+        throw Error(`Invalid network for v2 market. network: ${network}`);
     }
     const repayEvent = v2PoolContract.filters.Repay(getUnderlyingToken(to), null, null, null);
     const supplyEvent = v2PoolContract.filters.Deposit(getUnderlyingToken(to), null, null, null, null);
@@ -150,6 +181,8 @@ async function fetchTxns(
       switch (aTokenMarket) {
         case AaveMarket.v2:
           return await getV2ATokensEventsToFilterOut(fromBlock, toBlock);
+        case AaveMarket.v2Amm:
+          return await getV2ATokensEventsToFilterOut(fromBlock, toBlock, true);
         case AaveMarket.v3:
           return await getV3ATokensEventsToFilterOut(fromBlock, toBlock);
         default:
@@ -179,6 +212,21 @@ async function fetchTxns(
     } else if (network == ChainId.avalanche && aTokenMarket == AaveMarket.v2) {
       const tokenSymbol = Object.keys(V2_AVA_A_TOKENS).find(key => V2_AVA_A_TOKENS[key as keyof typeof V2_AVA_A_TOKENS] === aToken);
       return TOKENS_AVA[tokenSymbol as keyof typeof TOKENS_AVA];
+    } else if (network == ChainId.avalanche && aTokenMarket == AaveMarket.v3) {
+      const tokenSymbol = Object.keys(V3_AVA_A_TOKENS).find(key => V3_AVA_A_TOKENS[key as keyof typeof V3_AVA_A_TOKENS] === aToken);
+      return TOKENS_AVA[tokenSymbol as keyof typeof TOKENS_AVA];
+    } else if (network == ChainId.optimism && aTokenMarket == AaveMarket.v3) {
+      const tokenSymbol = Object.keys(V3_OPT_A_TOKENS).find(key => V3_OPT_A_TOKENS[key as keyof typeof V3_OPT_A_TOKENS] === aToken);
+      return TOKENS_OPT[tokenSymbol as keyof typeof TOKENS_OPT];
+    } else if (network == ChainId.arbitrum_one && aTokenMarket == AaveMarket.v3) {
+      const tokenSymbol = Object.keys(V3_ARB_A_TOKENS).find(key => V3_ARB_A_TOKENS[key as keyof typeof V3_ARB_A_TOKENS] === aToken);
+      return TOKENS_ARB[tokenSymbol as keyof typeof TOKENS_ARB];
+    } else if (network == ChainId.harmony && aTokenMarket == AaveMarket.v3) {
+      const tokenSymbol = Object.keys(V3_HAR_A_TOKENS).find(key => V3_HAR_A_TOKENS[key as keyof typeof V3_HAR_A_TOKENS] === aToken);
+      return TOKENS_HAR[tokenSymbol as keyof typeof TOKENS_HAR];
+    } else if (network == ChainId.fantom && aTokenMarket == AaveMarket.v3) {
+      const tokenSymbol = Object.keys(V3_FAN_A_TOKENS).find(key => V3_FAN_A_TOKENS[key as keyof typeof V3_FAN_A_TOKENS] === aToken);
+      return TOKENS_FAN[tokenSymbol as keyof typeof TOKENS_FAN];
     }
     throw Error('Unable to find the underlying token for the aToken');
   }
@@ -188,10 +236,10 @@ async function fetchTxns(
       if (events.length == 0) return events;
       const eventsToFilter = await getEventsToFilterOut(fromBlock, toBlock);
       const filteredEvents = events.filter((event) => !eventsToFilter.some((poolEvents) => poolEvents.transactionHash === event.transactionHash));
-      // console.log('filtered events', filteredEvents);
+      console.log('filtered events', filteredEvents);
       return filteredEvents as Event[];
     } catch (error) {
-      console.log('error in filtering');
+      console.log('error in filtering', error);
       throw(error);
     }
   }
@@ -305,6 +353,7 @@ async function generateEthTokensMap() {
   tokenList.forEach(async (token) => {
       const tokenName = token[0];
       const tokenAddress = token[1];
+      const v2AmmAToken = V2AMM_ETH_A_TOKENS[tokenName as keyof typeof V2AMM_ETH_A_TOKENS];
       const v2AToken = V2_ETH_A_TOKENS[tokenName as keyof typeof V2_ETH_A_TOKENS];
       const v3AToken = V3_ETH_A_TOKENS[tokenName as keyof typeof V3_ETH_A_TOKENS];
 
@@ -324,9 +373,17 @@ async function generateEthTokensMap() {
         await generateAndSaveMap(mappedContracts, `ethereum_a${tokenName}`);
       }
 
-      // rescue v2 tokens sent to aToken contract
+      // rescue v2 and v3 tokens sent to v2 and v3 aToken contracts respectively
       const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
         await Promise.all([
+          v2AmmAToken ? fetchTxns(
+            tokenAddress,
+            v2AmmAToken,
+            ChainId.mainnet,
+            `v2${tokenName}-v2amm${tokenName}`,
+            true,
+            AaveMarket.v2Amm,
+          ): {},
           v2AToken ? fetchTxns(
             tokenAddress,
             v2AToken,
@@ -373,7 +430,7 @@ async function generatePolTokensMap() {
         await generateAndSaveMap(mappedContracts, `polygon_a${tokenName.toLocaleLowerCase()}`);
       }
 
-      // rescue v2 tokens sent to aToken contract
+      // rescue v2 and v3 tokens sent to v2 and v3 aToken contracts respectively
       const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
         await Promise.all([
           v2AToken ? fetchTxns(
@@ -404,8 +461,9 @@ async function generateAvaTokensMap() {
       const tokenName = token[0];
       const tokenAddress = token[1];
       const v2AToken = V2_AVA_A_TOKENS[tokenName as keyof typeof V2_AVA_A_TOKENS];
+      const v3AToken = V3_AVA_A_TOKENS[tokenName as keyof typeof V3_AVA_A_TOKENS];
 
-      // rescue v2 tokens sent to aToken contract
+      // rescue v2 and v3 tokens sent to v2 and v3 aToken contracts respectively
       const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
         await Promise.all([
           v2AToken ? fetchTxns(
@@ -415,9 +473,113 @@ async function generateAvaTokensMap() {
             `v2${tokenName}-v2a${tokenName}`,
             true,
             AaveMarket.v2
+          ): {},
+          v3AToken ? fetchTxns(
+            tokenAddress,
+            v3AToken,
+            ChainId.avalanche,
+            `v3${tokenName}-v3a${tokenName}`,
+            true,
+            AaveMarket.v3
           ): {}
         ]);
       await generateAndSaveMap(mappedContracts, 'avalanche_' + tokenName.toLocaleLowerCase());
+  });
+}
+
+async function generateOptTokensMap() {
+  const tokenList = Object.entries(TOKENS_OPT);
+
+  tokenList.forEach(async (token) => {
+      const tokenName = token[0];
+      const tokenAddress = token[1];
+      const v3AToken = V3_OPT_A_TOKENS[tokenName as keyof typeof V3_OPT_A_TOKENS];
+
+      // rescue v3 tokens sent to aToken contract
+      const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
+        await Promise.all([
+          v3AToken ? fetchTxns(
+            tokenAddress,
+            v3AToken,
+            ChainId.optimism,
+            `v3${tokenName}-v3a${tokenName}`,
+            true,
+            AaveMarket.v3
+          ): {}
+        ]);
+      await generateAndSaveMap(mappedContracts, 'optimism_' + tokenName.toLocaleLowerCase());
+  });
+}
+
+async function generateArbTokensMap() {
+  const tokenList = Object.entries(TOKENS_ARB);
+
+  tokenList.forEach(async (token) => {
+      const tokenName = token[0];
+      const tokenAddress = token[1];
+      const v3AToken = V3_ARB_A_TOKENS[tokenName as keyof typeof V3_ARB_A_TOKENS];
+
+      // rescue v3 tokens sent to aToken contract
+      const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
+        await Promise.all([
+          v3AToken ? fetchTxns(
+            tokenAddress,
+            v3AToken,
+            ChainId.arbitrum_one,
+            `v3${tokenName}-v3a${tokenName}`,
+            true,
+            AaveMarket.v3
+          ): {}
+        ]);
+      await generateAndSaveMap(mappedContracts, 'arbitrum_' + tokenName.toLocaleLowerCase());
+  });
+}
+
+async function generateHarTokensMap() {
+  const tokenList = Object.entries(TOKENS_HAR);
+
+  tokenList.forEach(async (token) => {
+      const tokenName = token[0];
+      const tokenAddress = token[1];
+      const v3AToken = V3_HAR_A_TOKENS[tokenName as keyof typeof V3_HAR_A_TOKENS];
+
+      // rescue v3 tokens sent to aToken contract
+      const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
+        await Promise.all([
+          v3AToken ? fetchTxns(
+            tokenAddress,
+            v3AToken,
+            ChainId.harmony,
+            `v3${tokenName}-v3a${tokenName}`,
+            true,
+            AaveMarket.v3
+          ): {}
+        ]);
+      await generateAndSaveMap(mappedContracts, 'harmony_' + tokenName.toLocaleLowerCase());
+  });
+}
+
+async function generateFanTokensMap() {
+  const tokenList = Object.entries(TOKENS_FAN);
+
+  tokenList.forEach(async (token) => {
+      const tokenName = token[0];
+      const tokenAddress = token[1];
+      const v3AToken = V3_FAN_A_TOKENS[tokenName as keyof typeof V3_FAN_A_TOKENS];
+
+      // rescue v3 tokens sent to aToken contract
+      const mappedContracts: (Record<string,{ amount: string; txHash: string[] }>)[] =
+        await Promise.all([
+          v3AToken ? fetchTxns(
+            tokenAddress,
+            v3AToken,
+            ChainId.fantom,
+            `v3${tokenName}-v3a${tokenName}`,
+            true,
+            AaveMarket.v3
+          ): {}
+        ]);
+      await generateAndSaveMap(mappedContracts, 'fantom_' + tokenName.toLocaleLowerCase());
   });
 }
 
@@ -427,6 +589,10 @@ async function phase2() {
   await generateEthTokensMap();
   await generatePolTokensMap();
   await generateAvaTokensMap();
+  await generateOptTokensMap();
+  await generateArbTokensMap();
+  await generateHarTokensMap();
+  await generateFanTokensMap();
 }
 
 phase2().then(() => console.log('phase 2 all finished'));
